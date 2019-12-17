fd95d3ed49b8715f7361b9eefb92f4dcf8937252
Joshua Mulliken <joshua@mulliken.net>
2019-12-16 22:40:01 -0500

    fix: Changed turn_on to hopefully maintain state

--- custom_components/wyzeapi/wyzeapi/wyzeapi_bulb.py
+++ custom_components/wyzeapi/wyzeapi/wyzeapi_bulb.py
@@ -7,18 +7,41 @@ class WyzeBulb():
 		self._device_mac = device_mac
 		self._friendly_name = friendly_name
 		self._state = state
-		self._brightness = None
-		self._colortemp = None
+		self._old_brightness = self._brightness = None
+		self._old_colortemp = self._colortemp = None
 		self._just_changed_state = False
 
 	def turn_on(self):
-		if (self._brightness != None and self._colortemp != None):
+		if (self._brightness != self._old_brightness and self._colortemp != self._old_colortemp):
 			url = 'https://api.wyzecam.com/app/v2/device/set_property_list'
 
 			brightness = self.translate(self._brightness, 0, 255, 1, 100)
 			colortemp = self.translate(self._colortemp, 500, 153, 2700, 6500)
 
-			payloads = [{
+			payload = {
+				"phone_id": self._device_id,
+				"property_list": [
+					{"pid": "P1501", "pvalue": brightness},
+					{"pid": "P1502", "pvalue": colortemp},
+					{"pid": "P3", "pvalue": "1"},
+				],
+				"device_model": "WLPA19",
+				"app_name": "com.hualai.WyzeCam",
+				"app_version": "2.6.62",
+				"sc": "01dd431d098546f9baf5233724fa2ee2",
+				"sv": "a8290b86080a481982b97045b8710611",
+				"device_mac": self._device_mac,
+				"app_ver": "com.hualai.WyzeCam___2.6.62",
+				"ts": "1575951274357",
+				"access_token": self._access_token
+			}
+
+		elif (self._brightness == self._old_brightness and self._colortemp != self._old_colortemp):
+			url = 'https://api.wyzecam.com/app/v2/device/set_property_list'
+
+			colortemp = self.translate(self._colortemp, 500, 153, 2700, 6500)
+
+			payload = {
 				"phone_id": self._device_id,
 				"property_list": [
 					{"pid": "P3", "pvalue": "1"},
@@ -33,51 +56,13 @@ class WyzeBulb():
 				"app_ver": "com.hualai.WyzeCam___2.6.62",
 				"ts": "1575951274357",
 				"access_token": self._access_token
-			},
-			{
-				"phone_id": self._device_id,
-				"property_list": [
-					{"pid": "P3", "pvalue": "1"},
-					{"pid": "P1501", "pvalue": brightness}
-				],
-				"device_model": "WLPA19",
-				"app_name": "com.hualai.WyzeCam",
-				"app_version": "2.6.62",
-				"sc": "01dd431d098546f9baf5233724fa2ee2",
-				"sv": "a8290b86080a481982b97045b8710611",
-				"device_mac": self._device_mac,
-				"app_ver": "com.hualai.WyzeCam___2.6.62",
-				"ts": "1575951274357",
-				"access_token": self._access_token
-			}]
-
-		elif (self._brightness == None and self._colortemp != None):
-			url = 'https://api.wyzecam.com/app/v2/device/set_property_list'
-
-			colortemp = self.translate(self._colortemp, 500, 153, 2700, 6500)
-
-			payloads = [{
-				"phone_id": self._device_id,
-				"property_list": [
-					{"pid": "P3", "pvalue": "1"},
-					{"pid": "P1502", "pvalue": colortemp}
-				],
-				"device_model": "WLPA19",
-				"app_name": "com.hualai.WyzeCam",
-				"app_version": "2.6.62",
-				"sc": "01dd431d098546f9baf5233724fa2ee2",
-				"sv": "a8290b86080a481982b97045b8710611",
-				"device_mac": self._device_mac,
-				"app_ver": "com.hualai.WyzeCam___2.6.62",
-				"ts": "1575951274357",
-				"access_token": self._access_token
-			}]
-		elif (self._brightness != None and self._colortemp == None):
+			}
+		elif (self._brightness != self._old_brightness and self._colortemp == self._old_colortemp):
 			url = 'https://api.wyzecam.com/app/v2/device/set_property_list'
 
 			brightness = self.translate(self._brightness, 0, 255, 1, 100)
 
-			payloads = [{
+			payload = {
 				"phone_id": self._device_id,
 				"property_list": [
 					{"pid": "P3", "pvalue": "1"},
@@ -92,11 +77,11 @@ class WyzeBulb():
 				"app_ver": "com.hualai.WyzeCam___2.6.62",
 				"ts": "1575951274357",
 				"access_token": self._access_token
-			}]
+			}
 		else:
 			url = 'https://api.wyzecam.com/app/v2/device/set_property'
 
-			payload = [{
+			payload = {
 				'phone_id': self._device_id,
 				'access_token': self._access_token,
 				'device_model': 'WLPA19',
@@ -107,11 +92,12 @@ class WyzeBulb():
 				'pvalue': "1",
 				'pid': 'P3',
 				'app_ver': 'com.hualai.WyzeCam___2.6.62'
-			}]
+			}
 
-		for payload in payloads:
-			data = do_request(url, payload, no_return=True)
+		data = do_request(url, payload, no_return=True)
 
+		self._old_brightness = self._brightness
+		self._old_colortemp = self._colortemp
 		self._state = True
 		self._just_changed_state = True
 
