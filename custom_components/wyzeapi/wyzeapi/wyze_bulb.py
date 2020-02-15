d09352c4c6cbe06684ff740e060bfd0fe3cb52cb
Joshua Mulliken <joshua@mulliken.net>
2020-02-14 23:37:55 -0500

    Remove await from turn on and turn off to see if it speeds up requests

--- custom_components/wyzeapi/wyzeapi/wyze_bulb.py
+++ custom_components/wyzeapi/wyzeapi/wyze_bulb.py
@@ -101,7 +101,7 @@ class WyzeBulb():
 				'app_ver': 'com.hualai.WyzeCam___2.6.62'
 			}
 
-		await self._api.async_do_request(url, payload)
+		self._api.async_do_request(url, payload)
 
 		self._state = True
 		self._just_changed_state = True
@@ -147,7 +147,7 @@ class WyzeBulb():
 			'app_ver': 'com.hualai.WyzeCam___2.6.62'
 		}
 
-		await self._api.async_do_request(url, payload)
+		self._api.async_do_request(url, payload)
 
 		self._state = False
 		self._just_changed_state = True
