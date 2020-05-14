9637e756978698f8f578721cae675b5e244affa0
jfarmer08 <allenfarmer1@gmail.com>
2020-05-14 17:41:41 -0400

    Support for Sensor polling (#47)
    * Support for Contacts
    
    Addded support for Contacts and motion also added support for RSSI in Switch and Light
    
    * Sync with main branch
    
    * Sync
    
    * Added support for sensors to be an option
    
    Added support for sensors to be an option
    
    * Add files via upload
    
    * Optional Config for Sensors.
    
    Now you choose what you want to have. By default Sensors are False and Light and Switch are True. Also These are optional.
    Sample Config
    wyzeapi:
      username: name@me.com
      password: somepassword
      sensors: true
      light: true
      switch: true
    
    * Wrong Place
    
    * Add Support for Options
    
    Now you choose what you want to have. By default Sensors are False and Light and Switch are true. Also These are optional.
    Sample Config
    wyzeapi:
      username: name@me.com
      password: somepassword
      sensors: true
      light: true
      switch: true
    
    * Polling update
    
    Setting polling down to 10 secounds.
    
    * Update Readme
    
    * Code Clean up and Support for Sensor Polling
    
    Added support for Sensor Polling at 5 secounds. Clean up logging. Add option to disable any device from showing in HA.
    
    * Added Support open Close State Time
    
    Added Support open Close State Time
    
    * Clean Up
    
    * Code Clean up
    
    * Code Clean up
    
    * Code Clean Up
    
    * Update wyze_sensor.py
    
    * Change Line Endings
    
    Co-authored-by: Allen Farmer <james.farmer@dormakaba.com>

--- custom_components/wyzeapi/wyzeapi/wyze_sensor.py
+++ custom_components/wyzeapi/wyzeapi/wyze_sensor.py
@@ -6,7 +6,7 @@ _LOGGER = logging.getLogger(__name__)
 
 class WyzeSensor():
     def __init__(self, api, device_mac, friendly_name, state, open_close_state_ts, voltage, rssi, device_model):
-        _LOGGER.debug("Farmer: Sensor " + device_mac + " " +friendly_name + " " + device_model + " " +voltage + " "+ rssi + " " +str(open_close_state_ts) +" " + "initializing.")
+        _LOGGER.debug("Sensor " + device_mac + " " +friendly_name + " " + "initializing..")
 
         self._api = api
         self._device_mac = device_mac
@@ -25,7 +25,7 @@ class WyzeSensor():
         return self._state
 
     async def async_update(self):
-        _LOGGER.debug("Farmer: Sensor " + self._friendly_name + " updating.")
+        _LOGGER.debug("Sensor " + self._friendly_name + " updating.")
         if self._just_changed_state == True:
             self._just_changed_state == False
         else:
@@ -43,7 +43,8 @@ class WyzeSensor():
                 "app_ver":"com.hualai.WyzeCam___2.6.62",
                 "phone_system_type":"1",
                 "ts":"1575955054511",
-                "access_token": self._api._access_token
+                "access_token": self._api._access_token,
+                "refresh_token": self._api._refresh_token
             }
 
             data = await self._api.async_do_request(url, payload)
@@ -52,6 +53,7 @@ class WyzeSensor():
                 if self._device_model =="PIR3U":
                     if item['pid'] == "P1302":
                         self._state = True if int(item['value']) == 1 else False
+                        self._open_close_state_ts = item['ts']
                     if item['pid'] == "P1304":
                        self._rssi = item['value']
                     if item['pid'] == "P1303":
@@ -59,6 +61,7 @@ class WyzeSensor():
                 if self._device_model == "DWS3U":
                     if item['pid'] == "P1301":
                         self._state = True if int(item['value']) == 1 else False
+                        self._open_close_state_ts = item['ts']
                     if item['pid'] == "P1304":
                        self._rssi = item['value']
                     if item['pid'] == "P1303":
