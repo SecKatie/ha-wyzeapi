502245d0a4ed5f976100daf5b00b102e7aca9ff0
jfarmer08 <allenfarmer1@gmail.com>
2020-05-14 17:41:41 -0400

    feat: Support for Sensor polling (#47)* Support for ContactsAddded support for Contacts and motion also added support for RSSI in Switch and Light* Sync with main branch* Sync* Added support for sensors to be an optionAdded support for sensors to be an option* Add files via upload* Optional Config for Sensors.Now you choose what you want to have. By default Sensors are False and Light and Switch are True. Also These are optional.Sample Configwyzeapi:  username: name@me.com  password: somepassword  sensors: true  light: true  switch: true* Wrong Place* Add Support for OptionsNow you choose what you want to have. By default Sensors are False and Light and Switch are true. Also These are optional.Sample Configwyzeapi:  username: name@me.com  password: somepassword  sensors: true  light: true  switch: true* Polling updateSetting polling down to 10 secounds.* Update Readme* Code Clean up and Support for Sensor PollingAdded support for Sensor Polling at 5 secounds. Clean up logging. Add option to disable any device from showing in HA.* Added Support open Close State TimeAdded Support open Close State Time* Clean Up* Code Clean up* Code Clean up* Code Clean Up* Update wyze_sensor.py* Change Line EndingsCo-authored-by: Allen Farmer <james.farmer@dormakaba.com>

--- custom_components/wyzeapi/__init__.py
+++ custom_components/wyzeapi/__init__.py
@@ -14,10 +14,17 @@ import homeassistant.helpers.config_validation as cv
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'wyzeapi'
+CONF_SENSORS = "sensors"
+CONF_LIGHT = "light"
+CONF_SWITCH = "switch"
+
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_USERNAME): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string
+        vol.Required(CONF_PASSWORD): cv.string,
+        vol.Optional(CONF_SENSORS, default=True): cv.boolean,
+        vol.Optional(CONF_LIGHT, default=True): cv.boolean,
+        vol.Optional(CONF_SWITCH, default=True): cv.boolean
     })
 }, extra=vol.ALLOW_EXTRA)
 
@@ -38,6 +45,10 @@ https://github.com/JoshuaMulliken/ha-wyzeapi/issues
                               config[DOMAIN].get(CONF_PASSWORD))
     await wyzeapi_account.async_init()
 
+    sensor_support = config[DOMAIN].get(CONF_SENSORS)
+    light_support = config[DOMAIN].get(CONF_LIGHT)
+    switch_support = config[DOMAIN].get(CONF_SWITCH)
+
     if not wyzeapi_account.is_valid_login():
         _LOGGER.error("Not connected to Wyze account. Unable to add devices. Check your configuration.")
         return False
@@ -53,9 +64,12 @@ https://github.com/JoshuaMulliken/ha-wyzeapi/issues
     # Start up lights and switch components
     if wyzeapi_devices:
         _LOGGER.debug("Starting WyzeApi components")
-        await discovery.async_load_platform(hass, "light", DOMAIN, {}, config)
-        await discovery.async_load_platform(hass, "switch", DOMAIN, {}, config)
-        await discovery.async_load_platform(hass, "binary_sensor", DOMAIN, {}, config)
+        if light_support == True:
+            await discovery.async_load_platform(hass, "light", DOMAIN, {}, config)
+        if switch_support == True:
+            await discovery.async_load_platform(hass, "switch", DOMAIN, {}, config)
+        if sensor_support == True:
+            await discovery.async_load_platform(hass, "binary_sensor", DOMAIN, {}, config)
     else:
         _LOGGER.error("WyzeApi authenticated but could not find any devices.")
 
--- custom_components/wyzeapi/binary_sensor.py
+++ custom_components/wyzeapi/binary_sensor.py
@@ -2,11 +2,14 @@
 
 """Platform for binary_sensor integration."""
 import logging
+from datetime import timedelta
+import time;
 import datetime
 from .wyzeapi.wyzeapi import WyzeApi
 from . import DOMAIN
 
 import voluptuous as vol
+import homeassistant.util.dt as dt_util
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import ATTR_ATTRIBUTION, ATTR_BATTERY_LEVEL, DEVICE_CLASS_TIMESTAMP
@@ -18,7 +21,23 @@ from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_DOOR
     )
 
+#Add to support quicker update time. Is this to Fast?
+SCAN_INTERVAL = timedelta(seconds=5)
+
 ATTRIBUTION = "Data provided by Wyze"
+ATTR_STATE ="state"
+ATTR_AVAILABLE = "available"
+ATTR_MAC = "mac"
+ATTR_RSSI = "rssi"
+ATTR_DEVICE_MODEL = "device model"
+ATTR_OPEN_SINCE = "Open since"
+ATTR_LAST_ACTION = "last_action"
+ATTR_NO_MOTION_SINCE = "No motion since"
+
+NO_CLOSE = "no_close"
+MOTION = "motion"
+NO_MOTION = "no_motion"
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,8 +46,7 @@ async def async_setup_platform(hass, config, add_entities, discovery_info=None):
     _LOGGER.debug("""FARMER: Creating new WyzeApi binary_sensor component""")
 
     # Add devices
-    _LOGGER.debug("Farmer: binary_sensor.py Add entities for")
-    add_entities(WyzeSensor(sensor) for sensor in await hass.data[DOMAIN]["wyzeapi_account"].async_list_sensor())
+    add_entities([WyzeSensor(sensor) for sensor in await hass.data[DOMAIN]["wyzeapi_account"].async_list_sensor()], True)
 
 class WyzeSensor(BinarySensorDevice):
     """Representation of a Wyze binary_sensor."""
@@ -42,13 +60,12 @@ class WyzeSensor(BinarySensorDevice):
         self._voltage = sensor._voltage
         self._rssi = sensor._rssi
         self._device_mac = sensor._device_mac
-        #self._open_close_state_ts = sensor._open_close_state_ts
+        self._open_close_state_ts = sensor._open_close_state_ts
         self._device_model = sensor._device_model
 
     @property
     def name(self):
         """Return the display name of this sensor."""
-        #self._name = "wyzeapi_"+self._device_mac+"_"+ self._name
         return self._name
 
     @property
@@ -73,23 +90,55 @@ class WyzeSensor(BinarySensorDevice):
     @property
     def device_state_attributes(self):
         """Return device attributes of the entity."""
+            #TODO: Show only if contact
+            #ATTR_OPEN_SINCE : "Open since"
+            #TODO: Show only if Motion
+            #ATTR_NO_MOTION_SINCE : "No motion since"
         return {
             ATTR_ATTRIBUTION: ATTRIBUTION,
-            "state": self._state,
-            "available": self._avaliable,
-            "mac": self._device_mac,
+            ATTR_STATE : self._state,
+            ATTR_AVAILABLE : self._avaliable,
+            ATTR_MAC : self._device_mac,
             ATTR_BATTERY_LEVEL: self._voltage,
-            #"""Convert epoch millisecond expressed in integer"""
-            #DEVICE_CLASS_TIMESTAMP: datetime.datetime.fromtimestamp(self._open_close_state_ts),
-            "rssi": self._rssi,
-            "device model": self._device_model
+            ATTR_RSSI: self._rssi,
+            ATTR_DEVICE_MODEL : self._device_model,
+            ATTR_LAST_ACTION : self.epoch_to_UTC()
         }
+
+    @property
+    def should_poll(self):
+        """We always want to poll for sensors."""
+        return True
+
+    def epoch_to_UTC(self):
+        #The code below is slicing, works but not on integers. 
+        #If you want to use it you can convert the number to str for slicing then convert it back to int. 
+        #It is not the best practice but it can be done as the following:
+        lastupdatetime1 = str(self._open_close_state_ts)
+        lastupdatetime2 = lastupdatetime1[:-3]
+        lastupdatetime3 = int(lastupdatetime2)
+        lastupdatetime4 = dt_util.utc_from_timestamp(float(lastupdatetime3))
+        return lastupdatetime4
+
+    def time_since_last_update(self):
+        #Feature use
+        return True
+
+    def no_motion_since(self):
+        #Feature use
+        return True
+
+    def open_since(self):
+        #Feature use
+        return True
 
     async def async_update(self):
         """Fetch new state data for this sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
+        _LOGGER.debug("""Binary Sesnors doing a update.""")
         await self._sensor.async_update()
         self._state = self._sensor._state
         self._rssi = self._sensor._rssi
         self._voltage = self._sensor._voltage
+        self._open_close_state_ts = self._sensor._open_close_state_ts
