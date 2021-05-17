1c58b4d890f8efe53742aef76ec64171703f6c56
faanskit <marcus.karlsson@usa.net>
2021-05-17 19:53:00 +0200

    Bumped branch to also support scenes for master branch

--- custom_components/wyzeapi/scene.py
+++ custom_components/wyzeapi/scene.py
@@ -21,7 +21,7 @@ SCAN_INTERVAL = timedelta(seconds=30)
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities):
     _LOGGER.debug("""Creating new WyzeApi group component""")
-    client = hass.data[DOMAIN][config_entry.entry_id]
+    client = hass.data[DOMAIN][config_entry.entry_id]["wyze_client"]
 
     def get_groups() -> List[Group]:
         try:
