3d9ba6c2df26fea594df3d12290d9562d1d24314
Yoinx <yoinx@graffiti.net>
2021-10-28 19:23:51 -0400

    __init__.py: correct linter raise-missing-from
    Since we don't care about the underlying traceback here, let's just go ahead and raise from None to cleanup the traceback and appease the linter.

--- custom_components/wyzeapi/__init__.py
+++ custom_components/wyzeapi/__init__.py
@@ -105,7 +105,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
         )
     except:
         _LOGGER.error("Wyzeapi: Could not login. Please re-login through integration configuration")
-        raise ConfigEntryAuthFailed("Unable to login, please re-login.")
+        raise ConfigEntryAuthFailed("Unable to login, please re-login.") from None
 
     hass.data[DOMAIN][config_entry.entry_id] = {CONF_CLIENT: client}
 
