8d8063fcd1858b7011010e10b64d3d22a5cabfd3
Yoinx <yoinx@graffiti.net>
2021-11-03 20:13:53 -0400

    feat: add battery sensor for Wyze Cam Outdoor
    If the switch is a Wyze Cam Outdoor (WVOD1) attach a battery sensor entity to it.

--- custom_components/wyzeapi/const.py
+++ custom_components/wyzeapi/const.py
@@ -7,4 +7,5 @@ ACCESS_TOKEN = "access_token"
 REFRESH_TOKEN = "refresh_token"
 REFRESH_TIME = "refresh_time"
 
-LOCK_UPDATED = "lock_updated"
+LOCK_UPDATED = f"{DOMAIN}.lock_updated"
+CAMERA_UPDATED = f"{DOMAIN}.camera_updated"
