76b3c6c85c7c283551136a22b681463730605cfb
Brian Rogers <brg468@hotmail.com>
2023-08-04 11:02:29 -0400

    Update sensor.py

--- custom_components/wyzeapi/sensor.py
+++ custom_components/wyzeapi/sensor.py
@@ -3,15 +3,27 @@
 """Platform for sensor integration."""
 
 import logging
+import json
 from typing import Any, Callable, List
+from datetime import datetime
 
 from wyzeapy import Wyzeapy
 from wyzeapy.services.camera_service import Camera
 from wyzeapy.services.lock_service import Lock
+from wyzeapy.services.switch_service import Switch, SwitchUsageService
 
-from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorStateClass,
+    RestoreSensor,
+)
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_ATTRIBUTION, PERCENTAGE
+from homeassistant.components.sensor import SensorDeviceClass
+from homeassistant.const import (
+    ATTR_ATTRIBUTION,
+    PERCENTAGE,
+    UnitOfEnergy,
+)
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers import device_registry as dr
@@ -22,11 +34,15 @@ from .token_manager import token_exception_handler
 _LOGGER = logging.getLogger(__name__)
 ATTRIBUTION = "Data provided by Wyze"
 CAMERAS_WITH_BATTERIES = ["WVOD1"]
+OUTDOOR_PLUGS = ["WLPPO"]
 
 
 @token_exception_handler
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry,
-                            async_add_entities: Callable[[List[Any], bool], None]) -> None:
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: Callable[[List[Any], bool], None],
+) -> None:
     """
     This function sets up the config_entry
 
@@ -41,17 +57,25 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry,
     # Get the list of locks so that we can create lock and keypad battery sensors
     lock_service = await client.lock_service
     camera_service = await client.camera_service
+    switch_usage_service = await client.switch_usage_service
 
     locks = await lock_service.get_locks()
     sensors = []
     for lock in locks:
         sensors.append(WyzeLockBatterySensor(lock, WyzeLockBatterySensor.LOCK_BATTERY))
-        sensors.append(WyzeLockBatterySensor(lock, WyzeLockBatterySensor.KEYPAD_BATTERY))
+        sensors.append(
+            WyzeLockBatterySensor(lock, WyzeLockBatterySensor.KEYPAD_BATTERY)
+        )
 
     cameras = await camera_service.get_cameras()
     for camera in cameras:
         if camera.product_model in CAMERAS_WITH_BATTERIES:
             sensors.append(WyzeCameraBatterySensor(camera))
+
+    plugs = await switch_usage_service.get_switches()
+    for plug in plugs:
+        if plug.product_model in OUTDOOR_PLUGS:
+            sensors.append(WyzePlugPowerSensor(plug, switch_usage_service))
 
     async_add_entities(sensors, True)
 
@@ -82,14 +106,17 @@ class WyzeLockBatterySensor(SensorEntity):
     @callback
     def handle_lock_update(self, lock: Lock) -> None:
         """
-            Helper function to
-            Enable lock when Keypad has battery and
-            Make it avaliable when either the lock battery or keypad battery exists
+        Helper function to
+        Enable lock when Keypad has battery and
+        Make it avaliable when either the lock battery or keypad battery exists
         """
         self._lock = lock
         if self._lock.raw_dict.get("power") and self._battery_type == self.LOCK_BATTERY:
             self._available = True
-        if self._lock.raw_dict.get("keypad", {}).get("power") and self._battery_type == self.KEYPAD_BATTERY:
+        if (
+            self._lock.raw_dict.get("keypad", {}).get("power")
+            and self._battery_type == self.KEYPAD_BATTERY
+        ):
             if self.enabled is False:
                 self.enabled = True
             self._available = True
@@ -168,6 +195,7 @@ class WyzeLockBatterySensor(SensorEntity):
 
 class WyzeCameraBatterySensor(SensorEntity):
     """Representation of a Wyze Camera Battery"""
+
     _attr_device_class = SensorDeviceClass.BATTERY
     _attr_native_unit_of_measurement = PERCENTAGE
 
@@ -226,3 +254,136 @@ class WyzeCameraBatterySensor(SensorEntity):
     @property
     def native_value(self):
         return self._camera.device_params.get("electricity")
+
+
+class WyzePlugPowerSensor(RestoreSensor):
+    """Respresents an Outdoor Plug Total Power Sensor."""
+
+    _attr_device_class = SensorDeviceClass.ENERGY
+    _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
+    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+    _previous_hour = None
+    _previous_value = None
+    _past_hours_previous_value = None
+    _current_value = 0
+    _past_hours_value = 0
+    _hourly_power_usage_added = 0
+
+    def __init__(
+        self, switch: Switch, switch_usage_service: SwitchUsageService
+    ) -> None:
+        """Initialize a power sensor."""
+        self._switch = switch
+        self._switch_usage_service = switch_usage_service
+        self._switch.usage_history = None
+
+    @property
+    def name(self) -> str:
+        """Get the name of the sensor."""
+        return "Total Power Usage"
+
+    @property
+    def unique_id(self):
+        """Get the unique ID of the sensor."""
+        return f"{self._switch.nickname}.power-{self._switch.mac}"
+
+    @property
+    def should_poll(self) -> bool:
+        """No polling needed."""
+        return False
+
+    @property
+    def device_info(self):
+        """Return the device info."""
+        return {
+            "identifiers": {(DOMAIN, self._switch.mac)},
+            "name": self._switch.nickname,
+        }
+
+    def update_power(self):
+        """Update the power sensor."""
+        _now = int(datetime.utcnow().hour)
+        self._hourly_power_usage_added = 0
+
+        if self._switch.usage_history:
+            _raw_data = self._switch.usage_history
+            _LOGGER.debug(_raw_data)
+            _current_day_list = json.loads(_raw_data[0]["data"])
+
+            if len(_raw_data) > 0:  # Confirm there is data
+                if _now == 0:  # Handle rolling to the next UTC day
+                    self._past_hours_value = _current_day_list[23] / 1000
+                    if len(_raw_data) > 1:  # New Day's value
+                        _next_day_list = json.loads(_raw_data[1]["data"])
+                        self._current_value = _next_day_list[_now] / 1000
+                    else:
+                        self._current_value = 0
+                else:
+                    self._past_hours_value = _current_day_list[_now - 1] / 1000
+                    self._current_value = _current_day_list[_now] / 1000
+
+                # Set inital values to current values on startup.
+                # Has to be done after we check for current or next UTC day
+                if self._previous_hour is None:
+                    self._previous_hour = _now
+                if self._past_hours_previous_value is None:
+                    self._past_hours_previous_value = self._past_hours_value
+                if self._previous_value is None:
+                    self._previous_value = self._current_value
+
+                if _now != self._previous_hour:  # New Hour
+                    if self._past_hours_value > self._previous_value:
+                        self._hourly_power_usage_added = (
+                            self._past_hours_value - self._previous_value
+                        )
+                    self._hourly_power_usage_added += self._current_value
+                    self._previous_value = self._current_value
+                    self._previous_hour = _now
+                    self._past_hours_previous_value = self._past_hours_value
+
+                else:  # Current Hour
+                    if self._current_value > self._previous_value:
+                        self._hourly_power_usage_added += round(
+                            self._current_value - self._previous_value, 3
+                        )
+                        self._previous_value = self._current_value
+
+                    if self._past_hours_value > self._past_hours_previous_value:
+                        self._hourly_power_usage_added += round(
+                            self._past_hours_value - self._past_hours_previous_value, 3
+                        )
+                        self._past_hours_previous_value = self._past_hours_value
+
+                _LOGGER.debug(
+                    "Total Value Added to device %s is %s",
+                    self._switch.mac,
+                    self._hourly_power_usage_added,
+                )
+
+        return self._hourly_power_usage_added
+
+    @callback
+    def async_update_callback(self, switch: Switch):
+        """Update the sensor's state."""
+        self._switch = switch
+        self.update_power()
+        self._attr_native_value += self._hourly_power_usage_added
+        self.async_write_ha_state()
+
+    async def async_added_to_hass(self) -> None:
+        """Register Updater for the sensor and get previous data."""
+        state = await self.async_get_last_sensor_data()
+        if state:
+            self._attr_native_value = state.native_value
+        else:
+            self._attr_native_value = 0
+        self._switch.callback_function = self.async_update_callback
+        self._switch_usage_service.register_updater(
+            self._switch, 120
+        )  # Every 2 minutes seems to work fine, probably could be longer
+        await self._switch_usage_service.start_update_manager()
+        return await super().async_added_to_hass()
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Remove updater."""
+        self._switch_usage_service.unregister_updater(self._switch)
