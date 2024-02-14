3bbd52383a80c05fdab6ddc132d0a40ed0504772
Brian Rogers <brg468@hotmail.com>
2024-02-14 11:14:14 -0500

    add notification switch

--- custom_components/wyzeapi/switch.py
+++ custom_components/wyzeapi/switch.py
@@ -343,14 +343,14 @@ class WyzeCameraNotificationSwitch(SwitchEntity):
         await self._service.turn_on_notifications(self._device)
 
         self._device.notify = True
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off the switch."""
         await self._service.turn_off_notifications(self._device)
 
         self._device.notify = False
-        self.async_schedule_update_ha_state()
+        self.async_write_ha_state()
 
     @property
     def name(self):
