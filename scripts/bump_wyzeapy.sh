3a2863dbdc63c5c9261f5b316c3629b2002cf284
Katie Mulliken <katie@mulliken.net>
2024-05-21 16:50:53 -0400

    chore(release): 0.1.25

---
+++ scripts/bump_wyzeapy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TARGET_VERSION=$1
+
+if [ -z "$TARGET_VERSION" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+# Set the next version as undesired
+NEXT_VERSION=$(echo $TARGET_VERSION | awk -F. '{print $1"."$2"."$3+1}')
+
+echo "Constraining version to between $TARGET_VERSION and $NEXT_VERSION"
+
+VERSION_IDENTITY="wyzeapy>=${TARGET_VERSION},<${NEXT_VERSION}"
+
+echo "Setting version to $VERSION_IDENTITY"
+
+poetry add $VERSION_IDENTITY
+
+tmpfile=$(mktemp)
+jq "(.requirements[] | select(. | contains(\"wyzeapy\"))) |= \"$VERSION_IDENTITY\"" custom_components/wyzeapi/manifest.json > "$tmpfile" && mv "$tmpfile" custom_components/wyzeapi/manifest.json
