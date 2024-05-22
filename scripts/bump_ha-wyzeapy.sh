9deaefd2a780573813164431bed37181923563a5
Katie Mulliken <katie@mulliken.net>
2024-05-22 11:05:51 -0400

    chore(release): 0.1.25 (#587)

---
+++ scripts/bump_ha-wyzeapy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2024 Katie Mulliken <katie@mulliken.net>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+TARGET_VERSION=$1
+
+if [ -z "$TARGET_VERSION" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+poetry version $TARGET_VERSION
+
+tmpfile=$(mktemp)
+jq "(.version = \"$TARGET_VERSION\")" custom_components/wyzeapi/manifest.json > "$tmpfile" && mv "$tmpfile" custom_components/wyzeapi/manifest.json
+
+git checkout -b release/$TARGET_VERSION
+
+git add .
+
+git commit -m "chore(release): $TARGET_VERSION"
+
+git push origin release/$TARGET_VERSION
